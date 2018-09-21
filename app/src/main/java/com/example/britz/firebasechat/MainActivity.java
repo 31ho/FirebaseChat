904d30518f0eca96966abee90d36e39c8574d86c
31ho <starting201@gmail.com>
2018-09-22 06:37:12 +0900

    닉네임 변경 추가

--- app/src/main/java/com/example/britz/firebasechat/MainActivity.java
+++ app/src/main/java/com/example/britz/firebasechat/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.britz.firebasechat;
 
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.example.britz.firebasechat.data.Data;
 import com.example.britz.firebasechat.data.Pref;
@@ -31,12 +38,51 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     EditText editText;
     Pref pref;
+    Toolbar toolbar;
+    TextView tView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         pref = Pref.getInstance(this);
         list = new ArrayList<Data>();
+
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        View bar = getLayoutInflater().inflate(R.layout.layout_toolbar, null);
+        getSupportActionBar().setCustomView(bar);
+        tView = (TextView) bar.findViewById(R.id.tool_text);
+        tView.setText(pref.getUserName());
+        tView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText etEdit = new EditText(getApplicationContext());
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("입력");
+                dialog.setView(etEdit);
+
+                dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String inputValue = etEdit.getText().toString();
+                        pref.setUserName(inputValue);
+                        tView.setText(inputValue);
+                        Toast.makeText(MainActivity.this, "변경되었습니다", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                dialog.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                dialog.show();
+
+            }
+        });
 
         recyclerView = (RecyclerView) findViewById(R.id.main_recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
