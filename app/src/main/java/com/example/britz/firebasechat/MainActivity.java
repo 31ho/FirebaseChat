d88567300e7c67709d9e77cd057656043e0d92de
31ho <starting201@gmail.com>
2018-09-21 17:07:36 +0900

    리사이클러뷰 xml파일 작성 후 어댑터 연결중

--- app/src/main/java/com/example/britz/firebasechat/MainActivity.java
+++ app/src/main/java/com/example/britz/firebasechat/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.britz.firebasechat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.main_recycler);
+
     }
 }
